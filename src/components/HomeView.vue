021e7e3090c12e3d6f84425143455361bbadc98c
cansoftwareist <cansoftwareist@gmail.com>
2022-12-01 12:53:29 +0100

    update1

---
+++ src/components/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="homeview">
+        <div id="home1" class="bg-image">
+            asdasdsada
+        </div>
+        <p>adkjakljakldj</p>
+    </div>
+</template>
+  
+<script>
+    export default {
+    name: 'HomeView'
+    }
+</script>
+<style lang="scss">
+#home1 {
+    background-image: url('@/assets/home/homefirst.png');
+    display: block;
+}
+</style>  
